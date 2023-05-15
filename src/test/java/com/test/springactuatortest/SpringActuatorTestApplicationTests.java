fd6ee00192e22b538725e96a0eda32bd14f8bdcb
kiryl.kostyleu <kiryl.kostyleu@sap.com>
2023-05-15 17:59:36 +0300

    Project init

---
+++ src/test/java/com/test/springactuatortest/SpringActuatorTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.springactuatortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
