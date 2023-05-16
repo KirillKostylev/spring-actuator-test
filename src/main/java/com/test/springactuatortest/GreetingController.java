4f41197052cf9aee8d2bcf845b7ce3f3fe1e7c02
aliaksandr.tsviatkou <aletsv@gmail.com>
2023-05-16 13:19:15 +0300

    Update path for greeting endpoint

--- src/main/java/com/test/springactuatortest/GreetingController.java
+++ src/main/java/com/test/springactuatortest/GreetingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1")
 public class GreetingController {
 
-    @GetMapping
+    @GetMapping("/greet")
     public String greet(){
         return "Hello World!";
     }
